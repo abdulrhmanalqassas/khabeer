9d6c8a1d46cf4632e849fd809726c0be45bcd1e5
abdulrhmanalqassas <46942535+abdulrhmanalqassas@users.noreply.github.com>
2023-03-11 22:56:44 +0200

    init filter

---
+++ src/components/FilterAll.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { watch, ref } from "vue"
+const emit = defineEmits(["TableFilter"])
+
+const key = ref("name")
+const filteringValue = ref(null)
+watch(filteringValue, () => console.log("::::::::::",filteringValue))
+
+
+function onFilter() {
+    emit('TableFilter', key.value , filteringValue.value);
+}
+
+</script>
+  
+<template>
+     <input type="text" v-model="filteringValue">
+    <button type="button" @click="onFilter">
+       Filter
+    </button>
+</template>
+   
+<style scoped>
+button {
+  margin: 0 0.5rem 0 0;
+}
+
+input {
+  margin: 0;
+}
+</style>
